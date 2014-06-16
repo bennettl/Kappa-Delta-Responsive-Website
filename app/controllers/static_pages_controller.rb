4b077742f2b5dec6bcc85bfec7fddf650446ea9d
Bennett Lee <bennettl908@yahoo.com>
2014-06-16 14:32:26 -0700

    Modified home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
+	
+	def home
+		# Find three latest news article
+		@news = News.limit(3).order("created_at DESC")
+	end
 
 	# Send the contact us message and redirect user
 	def contact_send
