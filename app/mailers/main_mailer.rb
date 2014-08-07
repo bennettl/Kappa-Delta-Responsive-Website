819efa88284cd83dd26173018bb9c97c4ed1cfbd
Bennett Lee <bennett@tapiture.com>
2014-08-07 00:05:16 -0700

    email obsure

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -5,7 +5,18 @@ class MainMailer < ActionMailer::Base
 		first_name 	= params[:first_name]
 		last_name 	=  params[:last_name]
 		name 		= "#{first_name} #{last_name}"
-		to 			= params[:email_to]
+		
+		case params[:email_to]
+			when  "webmaster"
+				to = "nathaniel.schier@gmail.com"
+			when "president"
+				to = "dbs02011@mymail.pomona.edu"
+			when "board"
+				to = "jaredmathismmb@gmail.com"
+			else
+				to = "kzw02011@mymail.pomona.edu"
+		end
+
 		from 		= name + "<" + params[:email_from] + ">"
 		message_text 	= "The following message is sent from KD's Contact Form: \n\n #{name} says \n\n" + params[:message]
 		# message_html 	= "The following message is sent from KD's Contact Form: <br /><br /> #{name} says  <br /><br />" + params[:message]
