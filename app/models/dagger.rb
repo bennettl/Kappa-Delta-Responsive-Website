b7012a84d0da99231f624cb99f3cb8a4e73b46bf
Bennett Lee <bennett@tapiture.com>
2014-08-17 19:19:00 -0700

    updates

--- app/models/dagger.rb
+++ app/models/dagger.rb
@@ -3,7 +3,9 @@ class Dagger < ActiveRecord::Base
 	# Attachement 
 	has_attached_file :pdf,
 						:url => ":s3_domain_url",
-	                    :path => ":rails_root/public/assets/daggers/:id/:style/:basename.:extension"
+	                    :path => ":rails_root/public/assets/daggers/:id/:style/:basename.:extension",
+	                    :s3_protocol => :https
+
  	validates_attachment_content_type :pdf, :content_type => ['application/pdf', 'application/msword', 'text/plain'], :if => :pdf_attached?
 
  	# Validation
